33865a530657d84926a8436798a6eba085990f93
matiusX <62626270+matiusX@users.noreply.github.com>
2020-04-08 09:21:26 -0400

    Update forca.py
    An index to give chances for player

--- forca.py
+++ forca.py
@@ -4,9 +4,12 @@ letters = ['_', '_', '_', '_', '_']
 hello
 '''
 count_hits = 0
+chances = 0
 
 died = False
 won = False
+acertou = False
+
 while not died and not won:
     count_letter = 0
     formatted_letters = ' '.join(letters)
@@ -15,15 +18,25 @@ while not died and not won:
     print(formatted_letters)
 
     chute = input("What's the letter?")
-    for letter in word:
-        if letter.lower().strip() == chute.lower().strip():
-            letters[count_letter] = letter
-            count_hits += 1
+    if chute in word:
+        for letter in word:
+            if letter.lower().strip() == chute.lower().strip():
+                letters[count_letter] = letter
+                count_hits += 1
+                acertou = True
 
-            if count_hits == len(letters):
-                won = True
-                print(word)
-                print('You win!!')
-        count_letter += 1
+                if count_hits == len(letters):
+                    won = True
+                    print(word)
+                    print('You win!!')
+            count_letter += 1
+    else:
+        chances += 1
+        if chances == 6:
+            print('The word was: {}'.format(word))
+            print('You lose!!')
+            died = True
+
+
 
 
