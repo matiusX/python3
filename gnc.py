9ce0997703a0b8184263a4ef3899048314d90d73
matiusX <62626270+matiusX@users.noreply.github.com>
2020-04-22 15:02:11 -0400

    numerical graph of clusters
    A graph to preview in how many groups a data can be separated

---
+++ gnc.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans 
+
+data = pd.read_csv('link_of_data')
+
+clusters = []
+
+max_iter = 10
+#you need to know max_iter before
+#can be test the max_iter for a lot of values
+
+for i in range(1, max_iter + 1):
+    kmeans = KMeans(n_clusters=i, random_state=1234)
+    kmeans.fit(data)
+    clusters.append((i,kmeans.inertia_,))
+plt.plot([w[0] for w in inertia],[w[1] for w in inertia], marker="X")
